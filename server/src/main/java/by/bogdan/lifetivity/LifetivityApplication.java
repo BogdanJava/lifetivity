fc98662ecebacc7f30042df159ebd3e68b63e749
Bogdan Shishkin <bogdanshishkin1998@gmail.com>
2018-06-27 16:52:37 +0300

    init

---
+++ server/src/main/java/by/bogdan/lifetivity/LifetivityApplication.java
@@ -0,0 +1,12 @@
+package by.bogdan.lifetivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifetivityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifetivityApplication.class, args);
+    }
+}
